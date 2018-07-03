0361b6999955bf0550cf4b5f6bb53b383ebc5eab
Anthony Carton <mr.carton@gmail.com>
2018-07-03 13:06:27 -0600

    Converted JS Event listeners
    Converted Event Listeners from jQ to vJS

--- designs.js
+++ designs.js
@@ -1,8 +1,9 @@
 // Start only after DOM is loaded
 document.addEventListener('DOMContentLoaded', function() {
   // Define variables
-  // TODO no jQ
-  const NEW_CELL = document.getElementsByTagName('td');
+  const NEW_ROW = "<tr></tr>";
+  const NEW_CELL = "<td></td>";
+
   let sizePicker = document.getElementById('sizePicker');
   let gridHeight = document.getElementById('inputHeight');
   let gridWidth = document.getElementById('inputWidth');
@@ -10,11 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
   let pixelCanvas = document.getElementById('pixelCanvas');
 
   // Add event listeners
-  sizePicker.submit(function(){
+  sizePicker.addEventListener('submit', function(event){
     // prevent the page from refreshing
-    event.preventDefault();
+    event.preventDefault(); //TODO is this right?
     // assign gridHeight and gridWidth the val() of their respective form values.
-    gridHeight = inputHeight.val();
+    gridHeight = inputHeight.val(); // TODO: What is the vJS equiv for .val()?
     gridWidth = inputWidth.val();
     // call makeGrid function
     makeGrid(gridHeight, gridWidth);
@@ -25,21 +26,20 @@ document.addEventListener('DOMContentLoaded', function() {
   // Set the size of the cross stitch canvas
   function makeGrid(n,m) {
     // remove all table children
-    document.getElementsByTagName('tbody').empty();
+    document.getElementsByTagName('tbody').empty(); //TODO replace empty()
     // set table to N x M, don't need to nest the loops
     for (i = 0; i < n; i++) {
-      // fill row with <td> cells
       // add a <tr> element under table
-      pixelCanvas.append("<tr></tr>"); // TODO better way to do this?
+      pixelCanvas.append(NEW_ROW); // TODO better way to do this?
     }
     for (j = 0; j < m; j++) {
       // TODO no jq
-      table.getElementsByTagName('tr').append( NEW_CELL.clone() );
+      document.getElementsByTagName('tr').append( NEW_CELL.clone() );
     }
   }
 
   // set the cell to gridColor on <td> click using .on, because .click will not bind to dynamically created events
-  pixelCanvas.//on("click", "td", function() {
+  pixelCanvas.addEventListener("click", function() {
     // $( this ).css( "background-color", gridColor.val()); <-- how do I do that w/o jq
     console.log("color changer is working: " + gridColor.val()); // use this to text
   });
