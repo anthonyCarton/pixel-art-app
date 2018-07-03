41a30f85844fd641f847cddb1eaec9dca6f04462
Anthony Carton <mr.carton@gmail.com>
2018-07-03 15:42:47 -0600

    Canvas size working

--- designs.js
+++ designs.js
@@ -1,46 +1,53 @@
 // Start only after DOM is loaded
 document.addEventListener('DOMContentLoaded', function() {
-  // Define variables
-  const ROW = document.createElement('tr');
-  const CELL = document.createElement('td');
-
-  let pickSubmit = document.querySelector('input[type=submit]');
+  // Define Global Variables
+  let formInput = document.querySelector('#sizePicker');
   let gridColor = document.querySelector('#colorPicker');
-  let pixelCanvas = document.querySelector('#pixelCanvas');
+  let pixelCanvas = document.querySelector('#pixelCanvas').firstElementChild;
 
   // Add event listeners
-  pickSubmit.addEventListener('click', function(event){
+  formInput.addEventListener('submit', function(event){
+    // prevent the page from refreshing
+    event.preventDefault();
+
     // Assign user input to variables
-    let gridHeight = document.querySelector('#inputHeight').value;
-    let gridWidth = document.querySelector('#inputWidth').value;
+    let gridHeight = formInput.querySelector('input[name=height]').value;
+    let gridWidth = formInput.querySelector('input[name=width]').value;
 
     function makeGrid(n,m) {
-      // remove all table children
-      document.getElementsByTagName('tbody'); //TODO replace empty()
+      // Define makeGrid variables
+      const ROW = '<tr></tr>';
+      const CELL = '<td></td>';
+
+      // TODO: remove all table children
+      console.log('clear table');
+      pixelCanvas.innerHTML = '';
+
       // set table to N x M, don't need to nest the loops
+
       for (i = 0; i < n; i++) {
+        console.log('add row');
         // add a <tr> element under table
-        pixelCanvas.appendChild(ROW); // TODO better way to do this?
-      }
-      for (j = 0; j < m; j++) {
-        // document.getElementsByTagName('tr').appendChild(CELL);
+        //document.table.appendChild(ROW); // TODO better way to do this?
+        pixelCanvas.insertAdjacentHTML('beforeend', ROW);
+        for (j = 0; j < m; j++) {
+          console.log('add cell');
+          // document.ROW.appendChild(CELL);
+          pixelCanvas.lastElementChild.insertAdjacentHTML('beforeend', CELL);
+        }
       }
     }
-
     makeGrid(gridHeight, gridWidth);
 
     // print to console to make sure this stuff is working
     console.log("h x w :: " + gridHeight + " x " + gridWidth);
-
-    // prevent the page from refreshing
-    event.preventDefault();
   });
 
   // set the cell to gridColor on <td> click using .on, because .click will not bind to dynamically created events
-  pixelCanvas.addEventListener("click", function() {
+  //pixelCanvas.addEventListener("click", function() {
     // $( this ).css( "background-color", gridColor.val()); <-- how do I do that w/o jq
-    console.log("color changer is working: " + gridColor.val()); // use this to text
-  });
+    //console.log("color changer is working: " + gridColor.val()); // use this to text
+  //});
 
   // all code before this
 });
