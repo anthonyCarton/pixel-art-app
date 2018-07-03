d38c7ec4332b10f43949ac95d6f1bcdaf1e10153
Anthony Carton <mr.carton@gmail.com>
2018-07-03 17:52:44 -0600

    Removed console.logs for clarity

--- designs.js
+++ designs.js
@@ -16,30 +16,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function makeGrid(n,m) {
       pixelCanvas.innerHTML = '';
-      console.log('table cleared');
       for (i = 1; i <= n; i++) {
         const ROW = document.createElement('tr');
         pixelCanvas.appendChild(ROW);
-        console.log(`added row #${i}`);
         for (j = 0; j < m; j++) {
           const CELL = document.createElement('td');
           let pixelRow = pixelCanvas.lastElementChild;
           pixelRow.appendChild(CELL);
-          console.log('added cell');
         }
       }
     }
     makeGrid(gridHeight, gridWidth);
-
-    // print to console
-    console.log(`h x w :: ${gridHeight} x ${gridWidth}`);
   });
 
   pixelCanvas.addEventListener("click", function(event) {
     gridColor = document.querySelector('#colorPicker').value;
-    console.log(`selected color is: ${gridColor}`);
     event.target.style.backgroundColor=gridColor;
   });
-
-  // all code before this
 });
